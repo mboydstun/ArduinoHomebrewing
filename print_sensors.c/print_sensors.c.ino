6bd00ee4d68d67681a1bfe097941082cabe992b5
Matt Boydstun <mboydstun@gmail.com>
2016-06-12 09:49:33 -0700

    Initial commit for arduino brewing on the ESP8266
    Simple script to read all attached sensors, and log the results to the
    serial connection.

---
+++ print_sensors.c/print_sensors.c.ino
@@ -0,0 +1,75 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define DEVICE_PIN D4
+#define READ_DELAY 5 * 1000
+
+OneWire  ds(DEVICE_PIN);
+DallasTemperature sensors(&ds);
+
+typedef struct  {
+  float tempf;
+  DeviceAddress address;
+} temperature;
+
+void setup(void) {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  sensors.begin();
+}
+
+void loop(void) {  
+  digitalWrite(LED_BUILTIN, LOW); // turn on LED
+  
+  temperature* temperatures = discoverOneWireDevices();
+  logFoundDevices(temperatures);
+  
+  free(temperatures);
+
+  digitalWrite(LED_BUILTIN, HIGH); // turn off LED
+  delay(READ_DELAY);
+}
+
+temperature *discoverOneWireDevices(void) {
+  ds.reset_search();
+  sensors.requestTemperatures(); // Send the command to get temperatures
+
+  int count = sensors.getDeviceCount();
+  temperature* temperatures = (temperature*)malloc(sizeof(temperature) * count);
+  
+  for(int i = 0; i < count; i++) {
+    temperature* t =  (temperature*)malloc(sizeof(temperature));
+    sensors.getAddress(t->address, 0);
+    t->tempf = sensors.getTempF(t->address);
+    temperatures[i] = *t;
+  }
+  
+  return temperatures;
+}
+
+void logFoundDevices(temperature* temperatures) {
+  Serial.print("Devices: ");
+  Serial.println(sizeof(temperatures) / sizeof(void*));
+
+  for(int i = 0; i < sizeof(temperatures) / sizeof(void*); i++) {
+    printAddress(temperatures[i].address);
+    Serial.print(" - ");
+    Serial.println(temperatures[i].tempf);
+  }
+
+  Serial.println("");
+}
+
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+
+
+
